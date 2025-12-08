deca9ac696f6cff5f6264bc158223d3baf872713
midudev <miduga@gmail.com>
2025-12-08 16:05:24 +0100

    feat: add turbopack configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const path = require('node:path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  turbopack: {
+    root: path.join(__dirname, '.'),
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
